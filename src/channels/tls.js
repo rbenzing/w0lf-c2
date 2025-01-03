c9fa6c660ff634d16d2d2d2f0f809ae234ce39df
rbenzing <me@russellbenzing.com>
2025-01-03 16:05:24 -0500

    add tls, http2 channels

--- src/channels/tls.js
+++ src/channels/tls.js
@@ -1,31 +1,137 @@
 const tls = require('node:tls');
+const fs = require('node:fs');
+const path = require('node:path');
+const { logInfo, logError } = require('../modules/logging');
+const config = require('../modules/config');
 
-const startTLSServer = (activeClients, config) => {
-    server = tls.createServer({
-        key: fs.readFileSync(path.join(__dirname, config.channels.tls.cert.key)),
-        cert: fs.readFileSync(path.join(__dirname, config.channels.tls.cert.cert)),
-        ciphers: config.channels.tls.ciphers,
-        honorCipherOrder: true,
-        minVersion: config.channels.tls.version, // Minimum TLS version
-        secureOptions: tls.constants.SSL_OP_NO_SSLv2 | tls.constants.SSL_OP_NO_SSLv3 | tls.constants.SSL_OP_NO_COMPRESSION, // Disable SSLv2, SSLv3, and compression
-    }, (socket) => {
-        // finish
-        socket.on('end', () => {
-            logInfo(`\nClient ${sessionId} disconnected. IP: ${client.address}`, logStream);
-            if (client) {
-                client.lastSeen = new Date();
-                client.active = false;
+// Create the TLS server
+const server = tls.createServer({
+    key: fs.readFileSync(path.join(__dirname, config.channels.tls.cert.key)),
+    cert: fs.readFileSync(path.join(__dirname, config.channels.tls.cert.cert)),
+    ciphers: config.channels.tls.ciphers,
+    honorCipherOrder: true,
+    minVersion: config.channels.tls.version, // Minimum TLS version
+    secureOptions: tls.constants.SSL_OP_NO_SSLv2 | tls.constants.SSL_OP_NO_SSLv3 | tls.constants.SSL_OP_NO_COMPRESSION, // Disable SSLv2, SSLv3, and compression
+}, (socket) => {
+    const { getSessionId } = require('../modules/encdec');
+    const { addClientSession } = require('../modules/clients');
+
+    const clientAddress = socket.remoteAddress || '1';
+    const sessionId = getSessionId(clientAddress);
+
+    // Add client session
+    addClientSession(sessionId, socket);
+
+    let clientBuffer = '';
+
+    socket.on('data', async (chunk) => {
+        const { upsertClientSession, getClient } = require('../modules/clients');
+        const { executeQueuedCommands } = require('../modules/queue');
+        const { decryptData } = require('../modules/encdec');
+        const { handleDownloadResponse, handleResponse, handleBeacon } = require('../modules/handlers');
+        
+        try {
+            const payloadStr = chunk.toString('utf8');
+            let client = getClient(sessionId);
+
+            if (payloadStr.length >= config.data.chunk_size || payloadStr.includes('--FIN--')) {
+                // Chunk mode
+                clientBuffer += payloadStr;
+                upsertClientSession(sessionId, { waiting: true, buffer: clientBuffer });
+                client = getClient(sessionId);
+
+                if (clientBuffer.includes('--FIN--')) {
+                    const bufferChunks = clientBuffer.replace('--FIN--', '');
+                    const decrypted = await decryptData(bufferChunks, client.sessionId);
+                    const parsed = JSON.parse(decrypted);
+                    const response = parsed.response;
+
+                    if (response.download) {
+                        await handleDownloadResponse(response);
+                    } else {
+                        await handleResponse(response);
+                    }
+
+                    clientBuffer = '';
+                    upsertClientSession(sessionId, { waiting: false, buffer: '' });
+                }
+            } else {
+                // Non-chunk mode
+                const decrypted = await decryptData(payloadStr, client.sessionId);
+                const parsed = JSON.parse(decrypted);
+                const response = parsed.response;
+
+                if (response.beacon) {
+                    handleBeacon(response, client);
+                    await executeQueuedCommands(client);
+                } else if (response.download) {
+                    await handleDownloadResponse(response);
+                } else if (response.error) {
+                    logError(response.error);
+                } else {
+                    await handleResponse(response);
+                }
             }
-        });
-        socket.on('error', (err) => {
-            logError(`\nClient ${client.sessionId} threw an error: ${err.message}. IP: ${client.address}`, logStream);
-            if (client) {
-                client.active = false;
-            }
-        });
+        } catch (err) {
+            logError(err.message);
+        }
+    });
+
+    socket.on('end', () => {
+        const { getClient } = require('../modules/clients');
+        const { upsertClientSession } = require('../modules/clients');
+
+        const client = getClient(sessionId);
+        logInfo(`\nClient ${sessionId} disconnected. IP: ${client.address}`);
+        upsertClientSession(sessionId, { lastSeen: new Date(), active: false });
+    });
+
+    socket.on('error', (err) => {
+        const { getClient } = require('../modules/clients');
+        const { upsertClientSession } = require('../modules/clients');
+
+        const client = getClient(sessionId);
+        logError(`\nClient ${sessionId} threw an error: ${err.message}. IP: ${client.address}`);
+        upsertClientSession(sessionId, { active: false });
+    });
+});
+
+/**
+ * Starts listening on the TLS server
+ */
+const listenTLSServer = () => {
+    server.listen(config.channels.tls.port, config.server.host, () => {
+        logInfo(`\nServer is listening on tls://${config.server.host}:${config.channels.tls.port}`);
+    });
+
+    server.on('error', (err) => {
+        logError(`\nServer threw an error: ${err.message}`);
+        server.close();
     });
 };
 
+/**
+ * Returns the server instance
+ * @returns Server
+ */
+const getServerInstance = () => {
+    return server;
+};
+
+/**
+ * Closes the TLS server connection
+ */
+const closeTLSServer = async () => {
+    if (server) {
+        server.close(() => {
+            logInfo('\nServer connection closed');
+            server.unref();
+        });
+    }
+};
+
 module.exports = {
-    startTLSServer
+    closeTLSServer,
+    getServerInstance,
+    listenTLSServer
 };
